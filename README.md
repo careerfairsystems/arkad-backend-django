b3ff2cd7453bfc1e18e99fa70c36be4403b69d59
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-02-19 20:21:27 +0100

    Now using postgres as database
    No longer using sqlite, reading secret_key, debug and postgres password from environment or .env file

---
+++ README.md
@@ -0,0 +1,10 @@
+# Arkad backend
+
+This backend uses django and postgres as the database
+
+### Required environment variables
+
+Required environment variables are:
+- SECRET_KEY (Set this to a long secret value)
+- DEBUG (Must be set to "False" in production)
+- POSTGRES_PASSWORD (The postgres database password for the user arkad_db_user)
--- arkad/arkad/settings.py
+++ arkad/arkad/settings.py
@@ -9,21 +9,23 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv(verbose=True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)uoe0#djq0uz0*ypboy%0q1&j(m9w&6n*hz)m0nw+_g^w!o8_-'
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+if DEBUG:
+    print("DEBUG ENABLED")
 
 ALLOWED_HOSTS: list[str] = []
 
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'arkad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'arkad',
+        'USER': 'arkad_db_user',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',  # Change if using a remote database
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
