8087dbce6f4e2dfc7c674ba4ba8c10a6f297a98e
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-02-19 18:46:15 +0100

    Can now create student sessions

--- arkad/student_sessions/api.py
+++ arkad/student_sessions/api.py
@@ -3,7 +3,8 @@ from ninja import Router
 
 from student_sessions.models import StudentSession
 from student_sessions.schema import StudentSessionListSchema, StudentSessionSchema, AvailableStudentSessionListSchema, \
-    AvailableStudentSessionSchema
+    AvailableStudentSessionSchema, CreateStudentSessionSchema
+from user_models.models import Company
 
 router = Router()
 
@@ -13,3 +14,12 @@ def get_available_student_sessions(request: HttpRequest):
     return AvailableStudentSessionListSchema(
         student_sessions=[AvailableStudentSessionSchema.from_orm(s) for s in sessions],
         numElements=len(sessions))
+
+@router.post("/create-student-session", response={406: str, 201: StudentSessionSchema})
+def create_student_session(request: HttpRequest, session: CreateStudentSessionSchema):
+    data: dict = session.model_dump()
+    try:
+        data["company"] = Company.objects.get(id=data.pop("company_id"))
+    except Company.DoesNotExist:
+        return 406, "Company not found"
+    return 201, StudentSession.objects.create(**data)
