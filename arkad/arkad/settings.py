careerfairsystems/arkad-backend-django

commit e7bc80d03b75d8f036c5f04f2e4f35ceb22ac5b9

Deployement setup, docker + traefik (#2)
* initial setup of docker container

* Updates to use gunicorn and nginx

* Setting static and media in different way

* Docker setup now works correctly

* Updated the readme to clarify usage

* Fixed allowed_hosts

* Removed duplicated static url

* simple deployment structure

* removed version

* removed binding for port 80

* added traefik config

* removed file config for traefik

* moved labels to right container

* changed postgres to be persistent

* enable traefik

* added port binding for debug

* fixed nginx

* added postgres data to gitignore

* fixed nginx placement

---------

Co-authored-by: Ludvig Lindholm <ludviglindholm127@gmail.com>

arkad/arkad/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -24,69 +25,75 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+ALLOWED_HOSTS: list[str] = ["backend.arkadtlth.se", "staging.backend.arkadtlth.se"]
 if DEBUG:
+    ALLOWED_HOSTS.append("127.0.0.1")
     print("DEBUG ENABLED")
+
 MEDIA_ROOT: Path = BASE_DIR / "media"
 MEDIA_URL: str = "/media/"
 ALLOWED_HOSTS: list[str] = []
 
 
-# Application definition
+
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "media"
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'user_models',
-    'student_sessions',
-    'event_booking'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "user_models",
+    "student_sessions",
+    "event_booking",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'arkad.urls'
+ROOT_URLCONF = "arkad.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'arkad.wsgi.application'
+WSGI_APPLICATION = "arkad.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'arkad',
-        'USER': 'arkad_db_user',
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',  # Change if using a remote database
-        'PORT': '5432',       # Default PostgreSQL port
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "arkad",
+        "USER": "arkad_db_user",
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),  # Change if using a remote database
+        "PORT": "5432",  # Default PostgreSQL port
     }
 }
 
@@ -96,16 +103,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -113,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -125,10 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'user_models.User'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "user_models.User"

.gitignore
@@ -3,4 +3,7 @@
 venv
 .venv
 .env
+**/__pycache__
+db_data
+**/postgres_data
 arkad/media/

Dockerfile
@@ -1,0 +1,31 @@
+# Use Python 3.11 slim image as base
+FROM python:3.13-slim
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE=1 \
+  PYTHONUNBUFFERED=1
+
+# Install system dependencies
+RUN apt-get update \
+  && apt-get install -y --no-install-recommends \
+  postgresql-client \
+  build-essential \
+  && rm -rf /var/lib/apt/lists/*
+
+# Upgrade pip
+RUN pip install --no-cache-dir --upgrade pip
+
+# Copy requirements file
+COPY requirements.txt .
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./arkad /app/arkad
+
+WORKDIR /app/arkad

deployment/compose.yml
@@ -1,0 +1,37 @@
+volumes:
+  traefik-ssl-certs:
+    driver: local
+  traefik-ssl-acme:
+    driver: local
+networks:
+  traefik:
+    external: true
+services:
+  traefik:
+    image: traefik:v2.10.4
+    container_name: traefik
+    networks:
+      - traefik
+    ports:
+      - 80:80
+      - 443:443
+      - 127.0.0.1:8080:8080
+    volumes:
+      - ./traefik:/etc/traefik
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+      - traefik-ssl-certs:/ssl-certs
+      - traefik-ssl-acme:/ssl-acme
+    restart: unless-stopped
+    extra_hosts:
+      - "host.docker.internal:host-gateway"
+  # node_exporter:
+  #   image: quay.io/prometheus/node-exporter:latest
+  #   container_name: node_exporter
+  #   ports:
+  #     - 9100:9100
+  #   command:
+  #     - "--path.rootfs=/host"
+  #   pid: host
+  #   restart: unless-stopped
+  #   volumes:
+  #     - "/:/host:ro,rslave"

deployment/nginx/conf.d/arkad.conf
@@ -1,0 +1,20 @@
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://web:8000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    location /static/ {
+        alias /app/arkad/static/;
+    }
+
+    location /media/ {
+        alias /app/arkad/media/;
+    }
+}

deployment/staging/compose.yml
@@ -1,0 +1,69 @@
+services:
+  db:
+    image: postgres:16
+    volumes:
+      - ./postgres_data:/var/lib/postgresql/data/
+    environment:
+      - POSTGRES_USER=arkad_db_user
+      - POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+      - POSTGRES_DB=arkad
+    healthcheck:
+      test: ["CMD-SHELL", "pg_isready -U arkad_db_user -d arkad"]
+      interval: 5s
+      timeout: 5s
+      retries: 10
+    networks:
+      - app_network
+
+  web:
+    build: ../../
+    command: sh -c "
+      python manage.py collectstatic --no-input &&
+      python manage.py migrate &&
+      gunicorn --workers 8 --timeout 16 arkad.wsgi:application --bind 0.0.0.0:8000
+      "
+    volumes:
+      - static_volume:/app/arkad/static
+      - media_volume:/app/arkad/media
+    environment:
+      - DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}
+      - DEBUG=${DEBUG}
+      - POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+      - DB_HOST=db
+    depends_on:
+      db:
+        condition: service_healthy
+    networks:
+      - app_network
+
+  nginx:
+    image: nginx:latest
+    labels:
+      - "traefik.enable=true"
+      - "traefik.http.routers.app-staging.rule=Host(`staging.backend.arkadtlth.se`)"
+      - "traefik.http.routers.app-staging.entrypoints=websecure"
+      - "traefik.http.routers.app-staging.tls=true"
+      - "traefik.http.routers.app-staging.tls.certresolver=production"
+    volumes:
+      - static_volume:/app/arkad/static:ro
+      - media_volume:/app/arkad/media:ro
+      - ../nginx/conf.d:/etc/nginx/conf.d
+    ports:
+      - "8081:80"
+    expose:
+      - 80
+    depends_on:
+      - web
+    networks:
+      - app_network
+      - traefik
+
+volumes:
+  static_volume:
+  media_volume:
+
+networks:
+  app_network:
+    driver: bridge
+  traefik:
+    external: true

deployment/traefik/traefik.yaml
@@ -1,0 +1,44 @@
+global:
+  checkNewVersion: false
+  sendAnonymousUsage: false
+
+api:
+  dashboard: true
+  insecure: true
+
+# -- Change EntryPoints here...
+entryPoints:
+  web:
+    address: :80
+    # -- (Optional) Redirect all HTTP to HTTPS
+    http:
+      redirections:
+        entryPoint:
+          to: websecure
+          scheme: https
+  websecure:
+    address: :443
+
+# -- Configure your CertificateResolver here...
+certificatesResolvers:
+  staging:
+    acme:
+      email: hugo.e.persson@gmail.com
+      storage: /ssl-acme/acme.json
+      caServer: "https://acme-staging-v02.api.letsencrypt.org/directory"
+      # -- (Optional) Remove this section, when using DNS Challenge
+      httpChallenge:
+        entryPoint: web
+  production:
+    acme:
+      email: hugo.e.persson@gmail.com
+      storage: /ssl-acme/acme.json
+      caServer: "https://acme-v02.api.letsencrypt.org/directory"
+      # -- (Optional) Remove this section, when using DNS Challenge
+      httpChallenge:
+        entryPoint: web
+
+providers:
+  docker:
+    exposedByDefault: false
+    network: traefik

commit 3095b380111906ffbbf114856e57aa5014ba0a33

BUG: Fix for invalid allowed hosts
Fixes issue introduced from e7bc80d0

arkad/arkad/settings.py
@@ -31,12 +31,6 @@ if DEBUG:
     ALLOWED_HOSTS.append("127.0.0.1")
     print("DEBUG ENABLED")
 
-MEDIA_ROOT: Path = BASE_DIR / "media"
-MEDIA_URL: str = "/media/"
-ALLOWED_HOSTS: list[str] = []
-
-
-
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = BASE_DIR / "media"
 