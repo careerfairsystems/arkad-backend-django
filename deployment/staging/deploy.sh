5150880a21e508094865031ab385661e3feb4d44
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-05-07 16:01:28 +0200

    Remove unnecessary directory change in deploy script

--- deployment/staging/deploy.sh
+++ deployment/staging/deploy.sh
@@ -24,8 +24,6 @@ git pull
 LAST_COMMIT_HASH=$(git rev-parse HEAD)
 COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
-cd arkad
-
 # Build and start containers
 echo "Building Docker images..."
 docker compose build
