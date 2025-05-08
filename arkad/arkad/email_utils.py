1d8f7c9275b422169ee29aff59944e61a6b145bc
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-05-08 13:41:00 +0200

    In debug no longer crash if AWS environment key is not set

--- arkad/arkad/email_utils.py
+++ arkad/arkad/email_utils.py
@@ -1,5 +1,9 @@
+import logging
+
 import boto3  # type: ignore[import-untyped]
 from botocore.exceptions import ClientError  # type: ignore[import-untyped]
+
+from arkad.settings import DEBUG
 
 SENDER = "Arkad No Reply <no-reply@arkadtlth.se>"
 AWS_REGION = "us-west-2"
@@ -38,6 +42,13 @@ def send_mail(recipient_email: str, body_html: str, body_text: str, subject: str
     except ClientError as e:
         print(e.response["Error"]["Message"])
         raise e
+    except Exception as e:
+        if DEBUG:
+            logging.error(e)
+        else:
+            logging.error("An error occurred while sending the email.")
+            raise e
+
 
 
 if __name__ == "__main__":
