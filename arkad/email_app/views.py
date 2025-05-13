980144f07faddd29f032610fa26a0312685f7062
viviantram1 <vivian1194.vt@gmail.com>
2025-05-13 20:02:53 +0200

    Reset email också klart?

--- arkad/email_app/views.py
+++ arkad/email_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def test_view(request):
-    return render(request, 'email_app/sign_up.html', {"reset_link": "https://example.com/reset"})
+    return render(request, 'email_app/reset_email.html', {"reset_link": "https://example.com/reset", "name": "asdas"})
 
 # Create your views here.
