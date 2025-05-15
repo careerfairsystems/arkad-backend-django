2a5533083f2adf8d97e71a0b0c302be7b7fe51d3
lucas-mansson <lu5132ma-s@student.lu.se>
2025-05-15 22:19:09 +0200

    Changed reset_link so the reset password email works with custom template

--- arkad/email_app/views.py
+++ arkad/email_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def test_view(request):
-    return render(request, 'email_app/sign_up.html', {"reset_link": "https://example.com/reset", "name": "asdas"})
+    return render(request, 'email_app/sign_up.html', {"reset_link": "{{ protocol }}://{{ domain }}{% url 'password_reset_confirm' uidb64=uid token=token %}", "name": "asdas"})
 
 # Create your views here.
