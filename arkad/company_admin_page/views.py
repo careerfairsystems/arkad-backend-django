a41c003980631381ff2d7cfdf0149d63ef1c0535
lucas-mansson <lu5132ma-s@student.lu.se>
2025-05-26 19:07:43 +0200

    Started work on company admin page

--- arkad/company_admin_page/views.py
+++ arkad/company_admin_page/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+from student_sessions.models import StudentSession, StudentSessionApplication, StudentSessionTimeslot
+
 def company_admin_page(request):
+    
+    applications = StudentSessionApplication.objects.all()
+
+    #I application ska man kunna se: Namn, CV (rendera direkt i skärmen?), Motivational letter. Bild?
+
+    application_info = {
+        "name": "",
+        "cv": "",
+        "motivational_letter":""
+    }
+
     return render(
         request=request,
         template_name="company_admin_page.html",
-        context={}
+        context={"applications": applications}
     )
