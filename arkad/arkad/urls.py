923b995f451cbfe26173e60382428b5b436d444d
lucas-mansson <lu5132ma-s@student.lu.se>
2025-05-26 18:07:35 +0200

    Added url path to company pages

--- arkad/arkad/urls.py
+++ arkad/arkad/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
+    path("company/", include("company_admin_page.urls")),
 ]
