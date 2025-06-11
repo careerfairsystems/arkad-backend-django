1fae7a14193798f022ba6d5959753c60f51320a3
lucas-mansson <lu5132ma-s@student.lu.se>
2025-06-11 22:07:47 +0200

    Changes to the admin page for users, to add support for generating url to company admin page

--- arkad/user_models/admin.py
+++ arkad/user_models/admin.py
@@ -1,6 +1,24 @@
-from django.contrib import admin
-
+from django.contrib import admin, messages
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 # Register your models here.
-admin.site.register(User)
+#admin.site.register(User)
+
+
+#TODO: Add admin action for creating a URL with users jwt token. create jwt token with loong expiry
+@admin.action(description="Generate urls for companies.")
+def generate_urls(modeladmin, request, queryset) -> str:
+    updated = queryset.update(first_name="AnotherCompany")
+    #modeladmin.message_user(request, f"{updated}", messages.success)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ["id", "username", "email", "first_name", "last_name", "company", "is_active", "is_staff"]
+    list_filter = ["is_staff", "is_superuser", "is_active"]
+    actions = [generate_urls]
+
+
+#admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
