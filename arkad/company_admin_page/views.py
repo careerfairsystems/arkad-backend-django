9c780bba940fa3808b650b6af70de70a2d9f8062
lucas-mansson <lu5132ma-s@student.lu.se>
2025-06-26 19:42:52 +0200

    Now the a user can access the company admin page by decoding the jwt token from the url sent to the email

--- arkad/company_admin_page/views.py
+++ arkad/company_admin_page/views.py
@@ -1,25 +1,28 @@
+import jwt
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, render
 
 from user_models.models import User
 from companies.models import Company
 from student_sessions.models import StudentSession, StudentSessionApplication, StudentSessionTimeslot
+from arkad.jwt_utils import jwt_decode
+
 
 def company_admin_page(request, token):
-    #This is currently broken. 
-    #The token need to be decoded to extract the user id?
-    user = request.user
+
+    # Get user from jwt token given in the url
+    payload = jwt_decode(token)
+    user_id = payload.get("user_id")
+    user = User.objects.get(id=user_id)
 
     if not user.is_company:
         raise PermissionDenied("Insufficient permissions")
 
-    username = user.company.name
+    company_name = user.company.name
     
-    applications = StudentSessionApplication.objects.filter(student_session__company__name=username)
-    token = user.create_jwt_token()
+    applications = StudentSessionApplication.objects.filter(student_session__company__name=company_name)
 
-    #TODO: Fix so that "status" is visible on admin page
-
+    # TODO: Make this shorter
     application_info = []
     for application in applications:
         application_info.append({
@@ -34,7 +37,7 @@ def company_admin_page(request, token):
         request=request,
         template_name="company_admin_page.html",
         context={
-            "company_name": username,
+            "company_name": company_name, 
             "applications": application_info,
             "token": token
         }
