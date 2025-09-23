08048fdbd52cf77d672dc0f545e996e9f4399e7f
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-09-23 15:55:20 +0200

    CHORE: Enhance Sentry configuration with profiling options

--- arkad/arkad/settings.py
+++ arkad/arkad/settings.py
@@ -27,6 +27,12 @@ if not DEBUG:
         # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
         send_default_pii=True,
         traces_sample_rate=1.0,  # Adjust this value in production
+        # Set profile_session_sample_rate to 1.0 to profile 100%
+        # of profile sessions.
+        profile_session_sample_rate=1.0,
+        # Set profile_lifecycle to "trace" to automatically
+        # run the profiler on when there is an active transaction
+        profile_lifecycle="trace",
         enable_logs=True
     )
 
