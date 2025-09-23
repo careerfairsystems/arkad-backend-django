a80c8a30a5cfbc3bbd17dd1e9eac68fafb18ee6a
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-09-23 17:55:48 +0200

    BUG: Fix issue with asgi imports

--- arkad/arkad/asgi.py
+++ arkad/arkad/asgi.py
@@ -8,22 +8,28 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from arkad import routing
+
+from arkad.settings import DEBUG
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arkad.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arkad.settings")
 
 django_asgi_app = get_asgi_application()
+from arkad import routing  # noqa: E402
 
+asgi_settings = {
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    ),
+}
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-        ),
-    }
-)
+if not DEBUG:
+    del asgi_settings["http"]  # In production, we don't want to serve HTTP via ASGI
+
+application = ProtocolTypeRouter(asgi_settings)
