bccc1912ddfe506904ea89d203064bdece38d5db
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-09-23 20:11:35 +0200

    FEATURE: Enhance Company admin interface with search, display, and filter options

--- arkad/companies/admin.py
+++ arkad/companies/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from companies.models import Job, Company
 
 # Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'website', 'company_email']
+    list_filter = ['desired_degrees', ]
+
 admin.site.register(Job)
-admin.site.register(Company)
+admin.site.register(Company, CompanyAdmin)
