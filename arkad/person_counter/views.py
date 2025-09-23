0044e3efee4640413281495504dd9a5c5306538a
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-09-23 16:52:40 +0200

    REFACTOR: Add type hints to view functions for improved clarity

--- arkad/person_counter/views.py
+++ arkad/person_counter/views.py
@@ -1,11 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """Render the WebSocket demo interface"""
     return render(request, "person_counter/index.html")
 
 
-def room_counter(request):
+def room_counter(request: HttpRequest) -> HttpResponse:
     """Render the room counter demo interface"""
     return render(request, "person_counter/room_counter.html")
