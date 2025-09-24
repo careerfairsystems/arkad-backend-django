d0c6bff7b1fe4ccb843c09a2bff4d0388ba92285
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-09-24 14:04:16 +0200

    FEATURE: Update room listing endpoint to include counter values for authenticated staff users

--- arkad/person_counter/api.py
+++ arkad/person_counter/api.py
@@ -1,7 +1,7 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from django.http import HttpRequest
-from ninja.errors import HttpError
+from ninja.security import SessionAuthIsStaff
 
 from arkad.customized_django_ninja import Router
 from person_counter.models import RoomModel, PersonCounter
@@ -9,14 +9,16 @@ from person_counter.models import RoomModel, PersonCounter
 router = Router(tags=["Person Counter"])
 
 
-@router.get("rooms", auth=None)
+@router.get("rooms", auth=SessionAuthIsStaff())
 def list_rooms(request: HttpRequest):
-    """List available rooms with current counters."""
-    # Allow anyone (including anonymous) to fetch available rooms so the
-    # frontend can populate the dropdown for all users.
-    # If you want to restrict this later, add auth checks here.
+    """List available rooms.
 
-    rooms: List[Dict[str, int | str]] = []
+    Public endpoint: returns a list of rooms for everyone. If the requester is
+    an authenticated staff user, include the current counter value for each
+    room under the "counter" key. Non-staff/anonymous users receive objects
+    containing only the "name" field.
+    """
+    rooms: List[Dict[str, Union[str, int]]] = []
     for room in RoomModel.objects.all():
         last = PersonCounter.get_last(room.name)
         rooms.append({"name": room.name, "counter": last.count if last else 0})
