4fa12e1e59dbe6ed89fd3210db1f6470048f9087
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-09-29 17:26:24 +0200

    FEATURE: Update routing to make room counter the default view for authenticated staff users

--- arkad/person_counter/urls.py
+++ arkad/person_counter/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import index, room_counter
+from .views import room_counter
 
 urlpatterns = [
-    path("", index, name="person_counter"),
-    path("room-counter/", room_counter, name="room_counter_demo"),
+    path("", room_counter, name="person_counter"),
 ]
