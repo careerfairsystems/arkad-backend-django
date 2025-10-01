cd5e4f1b917be41e7edfb30ff050ea18d8ceef0e
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-01 18:29:51 +0200

    added outline functions for notifications run on a schedule

--- arkad/notifications/tasks.py
+++ arkad/notifications/tasks.py
@@ -4,3 +4,52 @@ from arkad.celery import app
 @app.task(bind=True)
 def check_notifications(self):  # type: ignore[no-untyped-def]
     print("Checking notifications!")
+
+
+@app.task(bind=True)
+def check_events_closing_within_24h(self):
+    # Find all events with registration closing withing 24 hrs
+
+    # for event in events
+        # Notify all users about this
+        # Anmälan för YYY med XXX stänger imorgon. Kom ihåg att avboka din plats om du inte kan komma!
+        # Notis (+ Mail)
+    pass
+
+
+@app.task(bind=True)
+def check_registered_events_tomorrow(self):
+    # Run once daily?
+    
+    # Get all events that are tomorrow
+
+    # for event in events
+        # Get company
+        # Find all users registered for that event
+        # for user in users
+            # Get their FCM token
+
+            # Send notification
+            # "Du har anmält dig till YYY med XXX är imorgon"
+            # - notis ( + mail)
+
+    pass
+
+
+@app.task(bind=True)
+def check_registered_events_in_one_hour(self):
+    # Run hourly at every whole hour between 07-17?
+    
+    # Find get all events that are in within 1h 
+
+    # for event in events
+        # Get company
+        # Find all users registered for that event
+
+        # for user in users
+            # Get their FCM token
+            # Send notification
+            # "Du har anmält dig till YYY som är med XXX är om en timme"
+            # - Notis
+    pass
+
