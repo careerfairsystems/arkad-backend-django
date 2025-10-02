ebf7e272e8b759553d5a082afb0ca2f0b41217ad
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-02 14:44:28 +0200

    renamed file with notification functions

--- arkad/notifications/tasks.py
+++ arkad/notifications/tasks.py
@@ -1,29 +1,37 @@
+#from arkad.celery import app
+from celery import shared_task
 from event_booking.models import Event
 from student_sessions.models import StudentSession, StudentSessionTimeslot
 from user_models.models import User
 
 # These are the tasks ot send notifications that are scheduled by scheduler.py
 
-def send_registration_closing_tomorrow(event: Event | StudentSession):
+@shared_task
+def notify_reg_close_tmrw(event: Event | StudentSession):
     # Defence companies (SAAB and FMV, any more?), append "swedish citizenship required"
     pass
 
 
-def send_event_tomorrow(user: User, event: Event | StudentSessionTimeslot):
+@shared_task
+def notify_event_tmrw(user: User, event: Event | StudentSessionTimeslot):
     pass
 
 
-def send_event_one_hour(user: User, event: Event | StudentSessionTimeslot):
+@shared_task
+def notify_event_one_hour(user: User, event: Event | StudentSessionTimeslot):
     pass
 
 
-def send_event_registration_open(event: Event | StudentSession):
+@shared_task
+def notify_event_reg_open(event: Event | StudentSession):
     # Both for lunch lectures, company visits (events?), and Student sessions
     #Anmälan för lunchföreläsning med XXX har öppnat -Bara notis
     # Anmälan för företagsbesök med XXX har öppnat - Bara notis
     pass
 
 
-def send_application_accepted(user: User, event: Event | StudentSession):
+@shared_task
+def notify_appl_accept(user: User, event: Event | StudentSession):
+    # Notify that a user has gotten their application accepted
     # Not a scheduled notification - this is triggered
     pass
