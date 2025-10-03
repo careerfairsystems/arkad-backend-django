acd4b4bee6d9a23bb16522ffa08777bf06d3c976
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-03 19:08:39 +0200

    rename notifications

--- arkad/notifications/tasks.py
+++ arkad/notifications/tasks.py
@@ -6,14 +6,9 @@ from user_models.models import User
 
 # These are the tasks ot send notifications that are scheduled by scheduler.py
 
-@shared_task
-def notify_reg_close_tmrw(event: Event | StudentSession):
-    # Defence companies (SAAB and FMV, any more?), append "swedish citizenship required"
-    pass
-
 
 @shared_task
-def notify_event_tmrw(user: User, event: Event | StudentSessionTimeslot):
+def notify_event_tmrw(user: User, event_name: str):
     pass
 
 
@@ -27,6 +22,12 @@ def notify_event_reg_open(event: Event | StudentSession):
     # Both for lunch lectures, company visits (events?), and Student sessions
     #Anmälan för lunchföreläsning med XXX har öppnat -Bara notis
     # Anmälan för företagsbesök med XXX har öppnat - Bara notis
+    pass
+
+
+@shared_task
+def notify_reg_close_tmrw(event: Event | StudentSession):
+    # Defence companies (SAAB and FMV, any more?), append "swedish citizenship required"
     pass
 
 
