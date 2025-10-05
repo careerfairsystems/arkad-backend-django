6d74e0d33deaa60e5cbe8f93d909eb44883b6e3b
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-05 19:16:15 +0200

    change endpoint function to more fitting name

--- arkad/notifications/api.py
+++ arkad/notifications/api.py
@@ -6,8 +6,8 @@ from notifications.schema import NotificationTokenSchema
 router = Router(tags=["Notifications"])
 
 
-@router.post("fcm-token", response={200: str})
-def get_fcm_token(request: AuthenticatedRequest, schema: NotificationTokenSchema):
+@router.post("fcm-token", response={ 200: str })
+def update_fcm_token(request: AuthenticatedRequest, schema: NotificationTokenSchema):
     request.user.fcm_token = schema.fcm_token  # user
     request.user.save()
     return 200, "Updated fcm token"
