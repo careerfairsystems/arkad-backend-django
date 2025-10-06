56406144f7fd98828663efdbcb0882289184b1d8
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-06 17:17:20 +0200

    fix lint

--- arkad/notifications/tasks.py
+++ arkad/notifications/tasks.py
@@ -3,31 +3,31 @@ from celery import shared_task  # type: ignore[import-untyped]
 from arkad.celery import app
 
 
-@shared_task
-def notify_event_tmrw(user_id: int, event_id: int) -> None: # type: ignore[no-untyped-def]
+@shared_task  # type: ignore
+def notify_event_tmrw(user_id: int, event_id: int) -> None:
     pass
 
 
-@shared_task
-def notify_event_one_hour(user_id: int, event_id: int) -> None: # type: ignore[no-untyped-def]
+@shared_task  # type: ignore
+def notify_event_one_hour(user_id: int, event_id: int) -> None:
     pass
 
 
-@shared_task
-def notify_event_reg_open(event_id: int) -> None: # type: ignore[no-untyped-def]
+@shared_task  # type: ignore
+def notify_event_reg_open(event_id: int) -> None:
     # Both for lunch lectures, company visits (events?), and Student sessions
     #Anmälan för lunchföreläsning med XXX har öppnat -Bara notis
     # Anmälan för företagsbesök med XXX har öppnat - Bara notis
     pass
 
 
-@shared_task
-def notify_reg_close_tmrw(event_id: int) -> None: # type: ignore[no-untyped-def]
+@shared_task  # type: ignore
+def notify_reg_close_tmrw(event_id: int) -> None:
     # Defence companies (SAAB and FMV, any more?), append "swedish citizenship required"
     pass
 
 
-@shared_task
+@shared_task  # type: ignore
 def notify_appl_accept(user_id: int, event_id: int) -> None:
     # Notify that a user has gotten their application accepted
     # Not a scheduled notification - this is triggered
