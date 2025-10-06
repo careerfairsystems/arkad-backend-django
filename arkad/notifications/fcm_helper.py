35ad774d208eb3c6bde8307ecd0eb8c038934e99
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-06 17:31:34 +0200

    fix lint

--- arkad/notifications/fcm_helper.py
+++ arkad/notifications/fcm_helper.py
@@ -9,7 +9,7 @@ from arkad import settings
 class FCMHelper:
     
     def __init__(self, cert_path: Path):
-        if cert_path and not firebase_admin._apps:
+        if not firebase_admin._apps:
             cred = credentials.Certificate(cert_path)
             firebase_admin.initialize_app(cred)
     
@@ -37,5 +37,5 @@ class FCMHelper:
         return str(response)
 
 cert_path = settings.BASE_DIR / "firebase_cert.json"
-if cert_path:
+if cert_path.exists():
     fcm = FCMHelper(cert_path)
