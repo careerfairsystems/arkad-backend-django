49ef602538fa3643f20cb5da5054776c969c6e9f
lucas-mansson <lu5132ma-s@student.lu.se>
2025-10-06 16:57:30 +0200

    fix lint?

--- arkad/notifications/tasks.py
+++ arkad/notifications/tasks.py
@@ -2,17 +2,17 @@ from celery import shared_task
 
 
 @shared_task
-def notify_event_tmrw(user_id: int, event_id: int):
+def notify_event_tmrw(user_id: int, event_id: int) -> None:
     pass
 
 
 @shared_task
-def notify_event_one_hour(user_id: int, event_id: int):
+def notify_event_one_hour(user_id: int, event_id: int) -> None:
     pass
 
 
 @shared_task
-def notify_event_reg_open(event_id: int):
+def notify_event_reg_open(event_id: int) -> None:
     # Both for lunch lectures, company visits (events?), and Student sessions
     #Anmälan för lunchföreläsning med XXX har öppnat -Bara notis
     # Anmälan för företagsbesök med XXX har öppnat - Bara notis
@@ -20,13 +20,13 @@ def notify_event_reg_open(event_id: int):
 
 
 @shared_task
-def notify_reg_close_tmrw(event_id: int):
+def notify_reg_close_tmrw(event_id: int) -> None:
     # Defence companies (SAAB and FMV, any more?), append "swedish citizenship required"
     pass
 
 
 @shared_task
-def notify_appl_accept(user_id: int, event_id: int):
+def notify_appl_accept(user_id: int, event_id: int) -> None:
     # Notify that a user has gotten their application accepted
     # Not a scheduled notification - this is triggered
     pass
