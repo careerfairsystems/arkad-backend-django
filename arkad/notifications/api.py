fd4dd23711204a57eea8e5d657950bbe9aa45c7c
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-10-14 17:43:02 +0200

    Refactor: Remove sent FCM to user by token endpoint

--- arkad/notifications/api.py
+++ arkad/notifications/api.py
@@ -1,7 +1,6 @@
 from arkad.customized_django_ninja import Router
 from user_models.models import AuthenticatedRequest
-from notifications.schema import UpdateFCMTokenSchema, NotifyByTokenSchema
-from notifications.fcm_helper import fcm
+from notifications.schema import UpdateFCMTokenSchema
 
 
 router = Router(tags=["Notifications"])
@@ -12,15 +11,3 @@ def update_fcm_token(request: AuthenticatedRequest, data: UpdateFCMTokenSchema):
     request.user.fcm_token = data.fcm_token  # user
     request.user.save()
     return 200, "Updated fcm token"
-
-
-@router.post("notify", response={200: str})
-def send_notification_to_token(
-    request: AuthenticatedRequest, data: NotifyByTokenSchema
-):
-    "Sends the specified message (title and body) to specified token."
-    if not request.user.is_superuser:
-        return 403, "Insufficient permissions"
-
-    fcm.send_to_user(token=data.token, title=data.title, body=data.body)
-    return 200, "Notification sent"
--- arkad/notifications/schema.py
+++ arkad/notifications/schema.py
@@ -3,9 +3,3 @@ from arkad.customized_django_ninja import Schema
 
 class UpdateFCMTokenSchema(Schema):
     fcm_token: str
-
-
-class NotifyByTokenSchema(Schema):
-    token: str
-    title: str
-    body: str
